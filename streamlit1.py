e13bdf64b289f26b43587bebaf3ef7ccfd88fa69
RandHamoud <168364455+RandHamoud@users.noreply.github.com>
2024-05-15 14:55:55 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -1,5 +1,3 @@
 import streamlit as st
 st.title("Which -----")
-st.write("I was interested to know what job opportunities are offered on "Jadarat" and which experiences have the highest chances in the market.
-"Jadarat" is the Unified Employment Platform (UEP). It is a national platform empowering the workforce of Saudi Arabia. Jobseekers can use the UEP to explore and apply for both public and private sector jobs in the Saudi labor market.
-")
+sr.write('I was interested to know what job opportunities are offered on "Jadarat" and which experiences have the highest chances in the market.')
