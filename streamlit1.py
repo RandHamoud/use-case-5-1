97518bce623eed3682bd59c1bfdb0daaf75fa95e
RandHamoud <168364455+RandHamoud@users.noreply.github.com>
2024-05-15 18:02:18 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Which Job Opportunities?")
-st.write('I was interested to know what job opportunities are offered on "Jadarat" and which experiences have the highest chances in the market.')
+st.write('I was interested to know what job opportunities are offered on **Jadarat** and which experiences have the highest chances in the market.')
 st.write('**Jadarat** is the Unified Employment Platform (UEP). It is a national platform empowering the workforce of Saudi Arabia. Jobseekers can use the UEP to explore and apply for both public and private sector jobs in the Saudi labor market.')
 
 st.write('There are 56.6% of positions on the website that are offered to **fresh graduates** based on the total number of available jobs on the website. **Since the majority of the jobs are for fresh graduate, I chose to analyze the market based on them.**')
@@ -26,4 +26,9 @@ st.write('Also the **small company** hire a fresh graduates.')
 file_path = 'chart4 -streamlit1.png'
 st.image(file_path, caption='', use_column_width=True)
 
-st.write('\n') 
+st.write('') 
+st.write('') 
+st.write('') 
+
+st.write('In my analysis, I examined the key factors that help you understand the job market for recent graduates. These factors include **expected salary, company type and size.**')
+st.write('**Good Luck!**')
