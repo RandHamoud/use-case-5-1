8f0c393cb047fc3781c962be800c974b41d45d12
RandHamoud <168364455+RandHamoud@users.noreply.github.com>
2024-05-15 17:51:40 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -6,6 +6,6 @@ st.write('**Jadarat** is the Unified Employment Platform (UEP). It is a national
 st.write('There are 56.6% of positions on the website that are offered to **fresh graduates** based on the total number of available jobs on the website. **Since the majority of the jobs are for fresh graduate, I chose to analyze the market based on them.**')
 file_path = 'chart1 -streamlit1.png'
 st.image(file_path, caption='', use_column_width=True)
-
+st.write('')
 st.write('The chart below shows the expected salary for fresh graduates, most companies offer them 4,000.')
 st.write('    - The range of salary between 3,000 to 12,000.')
